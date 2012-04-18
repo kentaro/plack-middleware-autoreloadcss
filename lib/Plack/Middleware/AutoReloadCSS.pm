01e5d4f8994290508ed7685d19e39a813ee22aa4
Kentaro Kuribayashi <kentarok@gmail.com>
2012-04-18 15:26:18 +0900

    initial import

---
+++ lib/Plack/Middleware/AutoReloadCSS.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::AutoReloadCSS;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::Middleware::AutoReloadCSS - blah blah blah
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::AutoReloadCSS;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::AutoReloadCSS is
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentarok@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Kentaro Kuribayashi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
